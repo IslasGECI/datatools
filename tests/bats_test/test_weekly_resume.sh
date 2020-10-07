cb16d6c1d0025ab585abf11ca628076f5e72a3f2
Maritza <maritza.bello@islas.org.mx>
2020-10-06 20:50:23 -0700

    Agrega función con prueba, que cuenta el total de capturas por zona

--- tests/bats_test/test_weekly_resume.sh
+++ tests/bats_test/test_weekly_resume.sh
@@ -5,8 +5,14 @@
   [ "$output" = '"hola mundo" adios' ]
 }
 
-@test "change header" {
+@test "change_header" {
   run cat tests/data_tests/tmp_header_IG_GATOS_30AGO2020.csv
   result="$(./src/change_header tests/data_tests/IG_POSICION_TRAMPAS_30AGO2020.csv)"
+  [ "$output" = "$result" ]
+}
+
+@test "get_captures_by_zone" {
+  run cat tests/data_tests/captures_by_zone.csv
+  result="$(./src/get_captures_by_zone tests/data_tests/tmp_header_IG_GATOS_30AGO2020.csv)"
   [ "$output" = "$result" ]
 }
