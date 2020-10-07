eec1d2616a11d9d30e7df9e7610f5ee06fbfacd6
Maritza <maritza.bello@islas.org.mx>
2020-10-06 21:20:07 -0700

    Cambia nombres de archivos de pruebas

--- tests/bats_test/test_weekly_resume.sh
+++ tests/bats_test/test_weekly_resume.sh
@@ -12,13 +12,13 @@
 }
 
 @test "get_captures_by_zone" {
-  run cat tests/data_tests/captures_by_zone.csv
+  run cat tests/data_tests/messy_captures_by_zone.csv
   result="$(./src/get_captures_by_zone tests/data_tests/tmp_header_IG_GATOS_30AGO2020.csv)"
   [ "$output" = "$result" ]
 }
 
 @test "get_effort_by_zone" {
-  run cat tests/data_tests/effort_by_zone.csv
+  run cat tests/data_tests/messy_effort_by_zone.csv
   result="$(./src/get_effort_by_zone tests/data_tests/tmp_header_IG_GATOS_30AGO2020.csv)"
   [ "$output" = "$result" ]
 }
