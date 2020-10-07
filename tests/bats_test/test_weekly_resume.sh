b1e0bc9b44b0eae832670353779712bb5e631d70
Maritza <maritza.bello@islas.org.mx>
2020-10-07 14:33:55 -0700

    Elimina función innecesaria que cuenta las capturas

--- tests/bats_test/test_weekly_resume.sh
+++ tests/bats_test/test_weekly_resume.sh
@@ -11,12 +11,6 @@
   [ "$output" = "$result" ]
 }
 
-@test "get_captures_by_zone" {
-  run cat tests/data_tests/messy_captures_by_zone.csv
-  result="$(./src/get_captures_by_zone tests/data_tests/tmp_header_IG_GATOS_30AGO2020.csv)"
-  [ "$output" = "$result" ]
-}
-
 @test "get_effort_by_zone" {
   run cat tests/data_tests/messy_effort_by_zone.csv
   result="$(./src/get_effort_by_zone tests/data_tests/tmp_header_IG_GATOS_30AGO2020.csv)"
