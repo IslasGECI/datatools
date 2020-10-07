1681160850e6a739707a411939d6413da7167708
Maritza <maritza.bello@islas.org.mx>
2020-10-06 20:08:44 -0700

    Agrega función con su archivo de prueba

--- tests/bats_test/test_weekly_resume.sh
+++ tests/bats_test/test_weekly_resume.sh
@@ -2,5 +2,11 @@
 
 @test "una oración" {
   run cat tests/data_tests/saludo.txt
-  [ "$output" = "hola mundo adios" ]
+  [ "$output" = '"hola mundo" adios' ]
+}
+
+@test "change header" {
+  run cat tests/data_tests/tmp_header_IG_GATOS_10MAY2020.csv
+  result="$(./src/change_header tests/data_tests/IG_POSICION_TRAMPAS_10MAY2020.csv)"
+  [ "$output" = "$result" ]
 }
