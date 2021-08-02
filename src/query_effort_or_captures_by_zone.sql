666981a6b8e66d7c0f877b436c64c5ff084ffd59
maritza <maritza.bello@islas.org.mx>
2021-08-02 15:31:12 -0700

    ✏️ Fix column names

--- src/query_effort_or_captures_by_zone.sql
+++ src/query_effort_or_captures_by_zone.sql
@@ -1,9 +1,9 @@
 SELECT
     '${fecha}' AS Fecha,
     CASE
-         WHEN ID = 'NA' THEN 'NA'
-         WHEN ID = 'SM4-Melpo' THEN '01'
-         ELSE SUBSTR(ID, 4, 2)
+        WHEN ID = 'NA' THEN 'NA'
+        WHEN ID = 'SM4-Melpo' THEN '01'
+        ELSE SUBSTR(ID, 4, 2)
     END AS Zona,
     COUNT(*) AS '${effort_or_captures}'
 FROM '${table_name}'
--- src/query_weekly_summary_effort_and_captures.sql
+++ src/query_weekly_summary_effort_and_captures.sql
@@ -1,16 +1,16 @@
 SELECT
-    d.real_date AS fecha,
-    jt.zona,
-    jt.esfuerzo,
+    d.real_date AS Fecha,
+    jt.Zona,
+    jt.Esfuerzo,
     CASE
-         WHEN jt.capturas IS NULL THEN '0' ELSE capturas
-    END AS capturas
+        WHEN jt.Capturas IS NULL THEN '0' ELSE Capturas
+    END Capturas
 FROM
     (SELECT
-        e.fecha AS tmp_fecha,
-        e.zona,
-        e.esfuerzo,
-        c.capturas
+        e.Fecha AS tmp_Fecha,
+        e.Zona,
+        e.Esfuerzo,
+        c.Capturas
     FROM '${effort_table}' AS e
   LEFT OUTER JOIN '${captures_table}' AS c
   ON e.Fecha = c.Fecha AND e.Zona = c.Zona) AS jt
