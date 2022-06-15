dd4e26ed9c483aec531f0791760e92cda3398df4
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-15 10:11:12 -0700

    🚧🚚 Read file with wrong format

--- src/fix_dates_socorro.R
+++ src/fix_dates_socorro.R
@@ -1,6 +1,8 @@
+#!/usr/bin/env Rscript
+
 library(tidyverse)
 
-file <- "tests/data_tests/correct_dates.csv"
+file <- "tests/data_tests/wrong_dates.csv"
 salida <- read_csv(file)
 write_csv(salida,"tests/data_tests/correct_dates.csv")
 
