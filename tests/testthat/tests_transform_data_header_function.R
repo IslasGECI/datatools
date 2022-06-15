3aecdb2c6768d2297330f565c56134c99aee614d
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-06-15 10:47:04 -0700

    ♻ Format R

--- tests/testthat/tests_transform_data_header_function.R
+++ tests/testthat/tests_transform_data_header_function.R
@@ -17,22 +17,25 @@ test_that("Adquiere fechas desde encabezado de base de datos", {
   expect_equal(transform_data_header(data_tests), output_tests)
 })
 
-test_that("Prueba cambio de formato en fecha",{
+test_that("Prueba cambio de formato en fecha", {
   expected_date <- "01/May/2022"
   obtained_date <- transform_date_format("2022-05-01")
-  expect_equal(expected_date,obtained_date)
+  expect_equal(expected_date, obtained_date)
   expected_date <- "02/May/2022"
   obtained_date <- transform_date_format("2022-05-02")
-  expect_equal(expected_date,obtained_date)
+  expect_equal(expected_date, obtained_date)
   expected_date <- "06/Ene/2022"
   obtained_date <- transform_date_format("2022-01-06")
-  expect_equal(expected_date,obtained_date)
+  expect_equal(expected_date, obtained_date)
   expected_date <- "06/Abr/2022"
   obtained_date <- transform_date_format("2022-04-06")
-  expect_equal(expected_date,obtained_date)
-  
-  raw_dates <- c("2022-05-01","2022-05-02","2022-01-06")
+  expect_equal(expected_date, obtained_date)
+
+  raw_dates <- c("2022-05-01", "2022-05-02", "2022-01-06")
   obtained_dates <- transform_date_format(raw_dates)
-  expected_dates <- c("01/May/2022","02/May/2022","06/Ene/2022")
-  expect_equal(expected_dates,obtained_dates)
+  expected_dates <- c("01/May/2022", "02/May/2022", "06/Ene/2022")
+  expect_equal(expected_dates, obtained_dates)
+})
+test_that("Lee header con fechas", {
+  file_wrong_format <- "tests/data_tests/wrong_dates.csv"
 })
