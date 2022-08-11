9bccc8b24b34d000f0c37d69e437030b09d476e0
Mario Villasante <mario.villasante@islas.org.mx>
2022-08-11 12:39:37 -0700

    ⚡️Add file path as argument

--- src/get_weekly_summary_socorro_from_excell.sh
+++ src/get_weekly_summary_socorro_from_excell.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 week=${1}
-in2csv --sheet "Hoja${week}" datos/junio.xlsx > tmp_csv
+file_path=${2}
+in2csv --sheet "Hoja${week}" ${file_path} > tmp_csv
 ./src/get_weekly_summary_socorro.sh tmp_csv
 mv weekly_summary_socorro_cleaned_format.csv week_${week}.csv
