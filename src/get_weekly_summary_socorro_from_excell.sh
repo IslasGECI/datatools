6c21f12b7394c1f901a94ef40465b9f591719949
Mario Villasante <mario.villasante@islas.org.mx>
2022-08-12 11:49:11 -0700

    🎨 Make format

--- src/get_weekly_summary_socorro_from_excell.sh
+++ src/get_weekly_summary_socorro_from_excell.sh
@@ -2,6 +2,6 @@
 
 week=${1}
 file_path=${2}
-in2csv --sheet "Hoja${week}" ${file_path} > tmp_csv
+in2csv --sheet "Hoja${week}" "${file_path}" > tmp_csv
 ./src/get_weekly_summary_socorro.sh tmp_csv
-mv weekly_summary_socorro_cleaned_format.csv week_${week}.csv
+mv weekly_summary_socorro_cleaned_format.csv "week_${week}.csv"
