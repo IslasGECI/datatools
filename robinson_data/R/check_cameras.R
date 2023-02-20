0abf8632479108eb77dc87e3d106f0d7828b2457
Guillermo Olvera • GECI <guillermo.olvera@islas.org.mx>
2023-02-20 10:33:41 -0600

    🩹 Add library to `read_csv()`

--- robinson_data/R/check_cameras.R
+++ robinson_data/R/check_cameras.R
@@ -7,7 +7,7 @@ search_cameras_in_list <- function(cameras_id, camera_coordinates_path) {
 }
 
 check_cameras <- function(camera_coordinates_path = "data/raw/robinson_coati_detection_camera_traps/camera_trap_coordinates.csv", raw_data_path = "data/raw/robinson_coati_detection_camera_traps/detection_camera_traps.csv") {
-  raw_data <- read_csv(raw_data_path, show_col_types = FALSE)
+  raw_data <- readr::read_csv(raw_data_path, show_col_types = FALSE)
   relative_path_column <- raw_data$RelativePath
   cameras_id <- robinson::get_id_camera_from_relative_path(relative_path_column)
   are_there_cameras_not_listed <- any(!search_cameras_in_list(cameras_id, camera_coordinates_path))
