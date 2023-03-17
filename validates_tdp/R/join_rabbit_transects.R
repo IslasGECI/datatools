7f6ec7876b7a53928370d1b555c9c33be6fc3d2f
Mario Villasante <mario.villasante@islas.org.mx>
2023-03-17 13:06:29 -0700

    ✅ Add species argument to 'write_transects_and_coordinates_table()'

--- validates_tdp/R/join_rabbit_transects.R
+++ validates_tdp/R/join_rabbit_transects.R
@@ -1,55 +1,101 @@
 #' @import dplyr
 
 #' @export
-write_transect_and_coordinates_table <- function(output_path, id_start, transect_path, coordinates_path){
-  transects_data <- read_csv(transect_path, show_col_types=FALSE)
-  coordinates_data <- read_csv(coordinates_path, show_col_types=FALSE)
-  joined_data <- join_coordinates_and_transects(transects_data, coordinates_data, id_start)
+write_transect_and_coordinates_table <- function(output_path, id_start, transect_path, coordinates_path, species = "Conejos") {
+  transects_data <- read_csv(transect_path, show_col_types = FALSE)
+  coordinates_data <- read_csv(coordinates_path, show_col_types = FALSE)
+  joined_data <- join_coordinates_and_transects(transects_data, coordinates_data, id_start, species)
   write_csv(joined_data, output_path)
 }
 
-join_coordinates_and_transects <- function(transects_data, coordinates_data, id_start = 1) {
+join_coordinates_and_transects <- function(transects_data, coordinates_data, id_start = 1, species = "Conejos") {
+  columns_for_species <- list(
+    "Conejos" = c(
+      "Id",
+      "Temporada",
+      "Fecha",
+      "Hora_inicio",
+      "Hora_final",
+      "Isla",
+      "Especie",
+      "Fase",
+      "Transecto",
+      "Area_isla",
+      "Cantidad_individuos",
+      "Longitud",
+      "Distancia",
+      "Tipo_de_vegetacion",
+      "Porcentaje_nubosidad",
+      "Velocidad_viento",
+      "Temperatura",
+      "Humedad",
+      "Observaciones"
+    ),
+    "Tecolotes" = c(
+      "Id",
+      "Fecha",
+      "Hora_inicio",
+      "Hora_final",
+      "Especie",
+      "Grupo",
+      "Transecto",
+      "Punto_del_transecto",
+      "Cantidad_individuos",
+      "Distancia",
+      "Individuos_fuera_de_monitoreo",
+      "Cantidad_aves_sobrevolando",
+      "Tipo_de_vegetacion",
+      "Porcentaje_nubosidad",
+      "Velocidad_viento",
+      "Temperatura",
+      "Humedad",
+      "Observaciones"
+    )
+  )
+  transects_data <- process_transect_data(transects_data, id_start, species)
+  coordinates_data <- process_coordinates_data(coordinates_data, species)
+  left_join(transects_data, coordinates_data, by = c("Transecto" = "Nombre_transecto"), multiple = "first") |>
+    select(columns_for_species[[species]])
+}
+
+process_transect_data <- function(transects_data, id_start, species) {
   transects_data <- transects_data |>
     mutate(Temporada = as.numeric(substring(Fecha, nchar(Fecha) - 3, nchar(Fecha)))) |>
     mutate(Id = assign_id(transects_data, id_start)) |>
     mutate(Fase = "Conteo/Diagnostico") |>
     mutate(Fecha = transform_date_format(Fecha, "%d/%m/%Y")) |>
-    mutate(Transecto = rename_transects(transects_data)) |>
+    mutate(Transecto = rename_transects(transects_data, species)) |>
+    mutate(Grupo = species) |>
+    mutate(Individuos_fuera_de_monitoreo = "NA") |>
+    mutate(Cantidad_aves_sobrevolando = "NA") |>
     rename(c(Cantidad_individuos = `# individuos`, Distancia = `Distancia (m)`, Tipo_de_vegetacion = `Tipo de Vegetación`))
-
-  coordinates_data <- coordinates_data |>
-    rename(Longitud = Longitud_transecto) |>
-    select(-c(Isla, Fecha, Temporada, Observaciones))
-
-  left_join(transects_data, coordinates_data, by = c("Transecto" = "Nombre_transecto"), multiple = "first") |>
-    select(c(
-      Id,
-      Temporada,
-      Fecha,
-      Hora_inicio,
-      Hora_final,
-      Isla,
-      Especie,
-      Fase,
-      Transecto,
-      Area_isla,
-      Cantidad_individuos,
-      Longitud,
-      Distancia,
-      Tipo_de_vegetacion,
-      Porcentaje_nubosidad,
-      Velocidad_viento,
-      Temperatura,
-      Humedad,
-      Observaciones
-    ))
 }
 
-rename_transects <- function(transects_data) {
-  paste_transect <- function(transect_number) {
-    paste0("Conejos_", stringr::str_pad(as.character(transect_number), 2, pad = "0"))
-  }
-  apply(transects_data["Transecto no."], 2, paste_transect)[, 1]
+process_coordinates_data <- function(coordinates_data, species = "Conejos") {
+  used_columns <- list(
+    "Conejos" = c(
+      "Nombre_transecto",
+      "Area_isla",
+      "Longitud"
+    ),
+    "Tecolotes" = c(
+      "Nombre_transecto",
+      "Area_isla",
+      "Longitud",
+      "Punto_del_transecto"
+    )
+  )
+  coordinates_data |>
+    rename(Longitud = Longitud_transecto) |>
+    select(all_of(used_columns[[species]]))
+}
+
+rename_transects <- function(transects_data, species = "Conejos") {
+  apply(transects_data["Transecto no."], 2, paste_transect, species = species)[, 1]
+}
+
+paste_transect <- function(transect_number, species) {
+  paste0(glue::glue("{species}_"), stringr::str_pad(as.character(transect_number), 2, pad = "0"))
 }
 
 assign_id <- function(transects_data, starting_id = 1) {
