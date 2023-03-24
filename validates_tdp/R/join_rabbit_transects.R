67ee6549672b16fe6d5c9c1fed8b2d2abed83a9a
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-03-23 17:04:03 -0700

    ✅ Pass test for 'Punto_del_transecto' in bird transects

--- validates_tdp/R/join_rabbit_transects.R
+++ validates_tdp/R/join_rabbit_transects.R
@@ -57,19 +57,44 @@ join_coordinates_and_transects <- function(transects_data, coordinates_data, id_
   )
   transects_data <- process_transect_data(transects_data, id_start, species)
   coordinates_data <- process_coordinates_data(coordinates_data, species)
-  left_join(transects_data, coordinates_data, by = c("Transecto" = "Nombre_transecto"), multiple = "first") |>
-    select(columns_for_species[[species]])
+  if (species == "Aves") {
+    return(left_join(transects_data, coordinates_data, by = c("Transecto" = "Nombre_transecto", "Punto_del_transecto" = "Punto_del_transecto"), multiple = "first") |>
+      select(columns_for_species[[species]]))
+  }
+  return(left_join(transects_data, coordinates_data, by = c("Transecto" = "Nombre_transecto"), multiple = "first") |>
+    select(columns_for_species[[species]]))
 }
 
 process_transect_data <- function(transects_data, id_start, species) {
   groups <- list("Conejos" = "Conejos", "Reptiles" = "Reptil", "Tecolotes" = "Tecolote", "Aves" = "Ave")
-  transects_data <- transects_data |>
+  if (species == "Aves") {
+    return(process_bird_transect(transects_data, id_start, species, groups))
+  }
+  return(process_native_except_bird_transect(transects_data, id_start, species, groups))
+}
+
+process_native_except_bird_transect <- function(transects_data, id_start, species, groups) {
+  transects_data |>
     mutate(Temporada = as.numeric(substring(Fecha, nchar(Fecha) - 3, nchar(Fecha)))) |>
     mutate(Id = assign_id(transects_data, id_start)) |>
     mutate(Fase = "Conteo/Diagnostico") |>
     mutate(Fecha = transform_date_format(Fecha, "%d/%m/%Y")) |>
     mutate(Transecto = rename_transects(transects_data, species)) |>
     mutate(Grupo = groups[[species]]) |>
+    rename(c(Cantidad_individuos = `# individuos`, Distancia = `Distancia (m)`, Tipo_de_vegetacion = `Tipo de Vegetación`)) |>
+    mutate(Individuos_fuera_de_monitoreo = "NA") |>
+    mutate(Cantidad_aves_sobrevolando = "NA")
+}
+
+process_bird_transect <- function(transects_data, id_start, species, groups) {
+  transects_data |>
+    mutate(Temporada = as.numeric(substring(Fecha, nchar(Fecha) - 3, nchar(Fecha)))) |>
+    mutate(Id = assign_id(transects_data, id_start)) |>
+    mutate(Fase = "Conteo/Diagnostico") |>
+    mutate(Fecha = transform_date_format(Fecha, "%d/%m/%Y")) |>
+    mutate(Transecto = rename_transects(transects_data, species)) |>
+    mutate(Grupo = groups[[species]]) |>
+    rename(`Punto_del_transecto` = `Punto no.`) |>
     rename(c(Cantidad_individuos = `# individuos`, Distancia = `Distancia (m)`, Tipo_de_vegetacion = `Tipo de Vegetación`)) |>
     mutate(Individuos_fuera_de_monitoreo = "NA") |>
     mutate(Cantidad_aves_sobrevolando = "NA")
